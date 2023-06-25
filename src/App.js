5c90e5504cfe0e64fc6539d45690f611733c1865
Mitchell Flood <mitchflood1@gmail.com>
2023-06-25 09:16:17 -0400

    adding existing skeleton for react component based project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>GPT-3</h1>
+  
+
+     <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+export default App
+
