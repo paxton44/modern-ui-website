cb2bf17141da2dc070cd417780414a44f46bb36a
Mitchell Flood <mitchflood1@gmail.com>
2023-06-26 14:22:01 -0400

    adding buttons to navbar/ styling buttons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,6 +11,13 @@ export const Navbar = () => {
         <div className='gpt3__navbar-links_logo' >
           <img src={logo} alt='logo' />
         </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+        </div>
       </div>
     </div>
   )
