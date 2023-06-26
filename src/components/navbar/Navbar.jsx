266c58e69b6e23739bce0e0c6960e92261926452
Mitchell Flood <mitchflood1@gmail.com>
2023-06-26 14:16:19 -0400

    beginning heavy lifting on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin, } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
 import './navbar.css';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    // Using BEM to style navbar (Block Element Modifier)
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo' >
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
