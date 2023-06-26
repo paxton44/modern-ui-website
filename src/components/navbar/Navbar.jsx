aac66821f19f8f09fe85607d2d95240c3d243315
Mitchell Flood <mitchflood1@gmail.com>
2023-06-26 16:02:23 -0400

    made mobile resposive and enabled menu animation on click for mobile devices

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,17 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css';
 
-export const Navbar = () => {
+  const Menu = () => (
+          <>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+          </>
+  )
+
+  const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false)
 
   return (
@@ -14,11 +24,7 @@ export const Navbar = () => {
           <img src={logo} alt='logo' />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#wgpt3'>What is GPT?</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case Studies</a></p>
-          <p><a href='#blog'>Library</a></p>
+          <Menu />
         </div>
         <div className='gpt3__navbar-sign'>
           <p>Sign In</p>
@@ -28,6 +34,19 @@ export const Navbar = () => {
           {toggleMenu
             ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
             : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+          }
+          {toggleMenu && (
+            <div className='gpt3__navbar-menu_container scale-up-center'>
+              <div className='gpt3__navbar-menu_container-links'>
+                <Menu />
+                <div className='gpt3__navbar-menu_container-links-sign'>
+          <p>Sign In</p>
+          <button type='button'>Sign Up</button>
+        </div>
+              </div>
+            </div>
+          )
+
           }
         </div>
       </div>
