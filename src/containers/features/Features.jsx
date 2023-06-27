fa167be39345e3d95b6edfc63a265b5b3fd400b1
Mitchell Flood <mitchflood1@gmail.com>
2023-06-27 15:36:56 -0400

    starting to build features.jsx and style

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,7 +3,11 @@ import './features.css';
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+      </div>
+    </div>
   )
 }
 
