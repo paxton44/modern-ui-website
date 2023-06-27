d38222d0523221e5b75a9053ed71b277e1d70442
Mitchell Flood <mitchflood1@gmail.com>
2023-06-27 14:18:48 -0400

    styling gpt3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpr3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+        <div className='gpt3__whatgpt3-container'>
+          <Feature />
+          <Feature />
+          <Feature />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
