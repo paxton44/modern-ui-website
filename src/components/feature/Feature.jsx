a4af2000a20f4ca1d6532516eed6ca8c016dd595
Mitchell Flood <mitchflood1@gmail.com>
2023-06-27 15:28:19 -0400

    fixed props title and text to fix styling then made whatgpt mobile responsive

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = ({title, text}) => {
-  return (
-    <div className='gpt3__features-container__feature'>
-      <div className='gpt3__features-container__feature-title'>
-        <div />
-        <h1>{title}</h1>
-        <div className='gpt3__features-container_feature-text'>
-          {text}
-        </div>
-      </div>
+const Feature = ({ title, text }) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
     </div>
-  )
-}
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
-export default Feature
+export default Feature;
