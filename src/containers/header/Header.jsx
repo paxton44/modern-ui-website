8cfd1acf2ac1a9602af861f8681bc41b7ca0f884
Mitchell Flood <mitchflood1@gmail.com>
2023-06-27 11:12:18 -0400

    adding images to header. needs styling to fit ai image

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import people from '../../assets/people.png'
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 import './header.css';
 
 const Header = () => {
@@ -15,7 +16,11 @@ const Header = () => {
           <button type='button'>Get Started</button>
         </div>
         <div className='gpt3__header-content__people'>
-          <img src={people} />
+          <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt='ai' />
         </div>
       </div>
     </div>
