764bec0155226cbfe7cb0c23db6878f4eb9c6434
Mitchell Flood <mitchflood1@gmail.com>
2023-06-27 16:30:58 -0400

    finished features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -20,20 +20,18 @@ const featuresData = [
   },
 ];
 
-const Features = () => {
-  return (
-    <div className='gpt3__features section__padding' id='features'>
-      <div className='gpt3__features-heading'>
-        <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
-        <p>Request Early Access To Get Started</p>
-        <div className='gpt3__features-container'>
-          {featuresData.map((item, index) => (
-            <Feature title={item.title} text={item.text} key={item.title + index} />
-          ))}
-        </div>
-      </div>
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <p>Request Early Access to Get Started</p>
     </div>
-  )
-}
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
 
 export default Features
