12b6392c8e259c396dfec4f7d83fe3aecd080c17
Mitchell Flood <mitchflood1@gmail.com>
2023-06-26 21:19:56 -0400

    starting header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text' >Let&apos;s Build Something
+          amazing with GPT-3 OpenAI
+        </h1>
+      </div>
+    </div>
   )
 }
 
