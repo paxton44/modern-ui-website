bcc391ca48ffcdd58c71cfcd8d30eb9797b92c2a
Mitchell Flood <mitchflood1@gmail.com>
2023-06-27 17:44:54 -0400

    made dent in blog need to finish / style and make articles work

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ imgUrl }) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+    </div>
   )
 }
 
