b63416d059a13e4388301c5b034f77c6e0941078
Mitchell Flood <mitchflood1@gmail.com>
2023-06-28 11:38:59 -0400

    finished blog starting footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
 import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section_padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do You Want To Step Into The 
+        Future Before Others?</h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p> 
+      </div>
+      <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+      <img src={gpt3Logo} alt="logo" />
+      </div>
+      </div>
+    </div>
   )
 }
 
